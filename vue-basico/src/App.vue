b878e48bb72efccaa01891416bc8dac647abb5f1
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-09-21 17:05:51 -0300

    navbar 🐱‍💻

--- vue-basico/src/App.vue
+++ vue-basico/src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <nav>
+    <Menu />
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </nav>
+    </nav> -->
     <router-view/>
   </div>
 </template>
 
+<script>
+
+import Menu from '@/components/menu/Menu.vue'
+
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+  :root {
+    --cor-primaria: #4FC3F7;
+    --cor-secundaria:#81D4FA;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Quicksand', sans-serif;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
