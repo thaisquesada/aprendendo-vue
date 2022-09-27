762780aaca59b3fc136dfcb9e5429adc4ff2b502
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-09-27 15:30:47 -0300

    criação de cards utilizando slots 🐱‍💻

---
+++ vue-basico/src/components/card/Card.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="card">
+        <img src="https://images.tcdn.com.br/img/img_prod/715047/caneta_esferografica_a_one_colors_colorida_molin_1001_1_7b2a2312a2bb6a4d87f54d9f10612545.jpg"
+        alt=""
+        />
+
+        <h4>
+            <slot name="nome"></slot>
+        </h4>
+
+        <p class="valor">
+            <slot name="valor"></slot>
+        </p>
+
+        <p>
+            <slot name="observacao"></slot>
+        </p>
+        
+        <div>
+            <slot name="acoes"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Card',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0. 0.3);
+        max-width: 300px;
+        margin: auto;
+        margin-bottom: 15px;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .valor {
+        color: #666666;
+        font-size: 22px;
+    }
+
+    .icones-tabela {
+        margin: 5px;
+        cursor: pointer;
+        color: var(--cor-primaria);
+}
+
+</style>
--- vue-basico/src/views/ListaProdutosCards.vue
+++ vue-basico/src/views/ListaProdutosCards.vue
@@ -1,5 +1,47 @@
 <template>
-    <div class="container">
-        <h1>Cards</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1 class="titulo">Cards de produtos</h1>
+        <hr />
+      </div>
     </div>
+
+    <div class="row mt-3">
+      <!-- Aqui criaremos os cards -->
+      <Card>
+        <template v-slot:nome> Produto B </template>
+
+        <template v-slot:valor> R$200,00 </template>
+
+        <template v-slot:observacao> Produto novo </template>
+
+        <template v-slot:acoes>
+          <i
+            @click="editarProduto(item)"
+            class="fas fa-solid fa-pen icones-tabela"
+          ></i>
+          <i
+            @click="excluirProduto(item)"
+            class="fas fa-solid fa-trash icones-tabela"
+          ></i>
+        </template>
+      </Card>
+    </div>
+  </div>
 </template>
+
+<script>
+import Card from "@/components/card/Card.vue";
+
+export default {
+  name: "ListaProdutoCards",
+  components: {
+    Card,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
